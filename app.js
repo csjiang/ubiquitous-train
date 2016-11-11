05b472e5103fbc2a904c2a75abf1eea04a60545f
Ya-wen Alice <yawenalice@gmail.com>
2016-11-11 18:02:10 -0500

    update

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
 var router = require('./routes')
 
-//templating setup 
+//templating setup
 var env = nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html'); //allows res.render to work w html files
-app.engine('html', nunjucks.render); //res.render uses nunjucks to render html 
+app.engine('html', nunjucks.render); //res.render uses nunjucks to render html
 
 //logging middleware
 app.use(morgan('tiny'));
@@ -24,10 +24,12 @@ app.listen(3000, function() {
 	console.log('Server listening on port 3000');
 });
 
+
+
 //static middleware
 app.use(express.static('/public'));
 
-//modular routing 
+//modular routing
 app.use('/', router)
 
 
